1eca990ba2cba480edadc910c400407be5b432f8
Hukum Yadav <hukumyadav45@gmail.com>
2023-09-15 11:35:45 +0530

    final commit

---
+++ JS/main.js
@@ -0,0 +1,26 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
+
+// To disable f12
+
+$(document).keydown(function(e){ 
+  if(e.which === 123){ 
+
+     return false; 
+
+  } 
+
+});
